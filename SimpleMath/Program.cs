cca992f6f47e456ec38dc5572d4d017025bef9fe
Le Tran Bao Duy <duyltbse182424@fpt.edu.vn>
2025-05-20 12:59:53 +0700

    new changes

---
+++ SimpleMath/Program.cs
@@ -0,0 +1,30 @@
+﻿string doMath(double a, double b, string op)
+{
+    string rs = "";
+    switch(op)
+    {
+        case "+":
+            rs += a+"+"+b;
+            break;
+        case "-":
+            rs += a + "+" + b;
+            break;
+        case "*":
+            rs += a + "+" + b;
+            break;
+        case "/":
+            rs += a + "+" + b;
+            break;
+        default:
+            rs = "fuking bullshit";
+            break;
+    }
+    return rs;
+}
+Console.WriteLine("Enter a:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter b");
+double b = Convert.ToDouble(Console.ReadLine());
+string op = Console.ReadLine();
+string result = doMath(a, b, op);
+Console.WriteLine(result);
---
+++ StringInteraction/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+string ho = "Nguyen";
+string tenLot = "Van";
+string full_name = ho + "" + tenLot;
+Console.WriteLine("Họ và tên: " + full_name);
+
+StringBuilder sb = new StringBuilder(); 
+sb.Append(ho); 
+sb.Append(" ");
+sb.Append(tenLot);
+Console.WriteLine(sb.ToString());
+
