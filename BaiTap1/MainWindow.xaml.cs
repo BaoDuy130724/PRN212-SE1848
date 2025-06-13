9aa4b51869634d11566fab3d6f21a60024aa7816
Le Tran Bao Duy <baoduy1372004@gmail.com>
2025-06-13 19:14:16 +0700

    lesson 13/6

---
+++ BaiTap1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BaiTap1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sum_click(object sender, RoutedEventArgs e)
+        {
+            var numberA = txtNumbera.Text == "" ? 0 : int.Parse(txtNumbera.Text);
+            var numberB = txtNumberb.Text == "" ? 0 : int.Parse(txtNumberb.Text);
+            var sum = numberA + numberB;
+            txtResult.Text = sum.ToString();
+            
+        }
+
+        private void exit_click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ HelloWPFApp/LoginWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWPFApp
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnDangNhap_Click(object sender, RoutedEventArgs e)
+        {
+            // Gia lap account la username: obama, password: 123
+            if(txtUsername.Text=="obama" && txtPassword.Password == "123")
+            {
+                MainWindow mw = new MainWindow();
+                mw.Show();
+                Close();
+                //hoac goi
+                //btnDangNhap.RaiseEvent();
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập thất bại rồi bro!!!!");
+            }
+        }
+    }
+}
