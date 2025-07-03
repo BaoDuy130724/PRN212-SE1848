1aa18122d2f15c37def2e3f3da88090cce2c6b92
Le Tran Bao Duy <baoduy1372004@gmail.com>
2025-07-03 17:45:42 +0700

    bai tap

---
+++ MyStoreWpfApp_EntityFramework/AdminWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using MyStoreWpfApp_EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyStoreWpfApp_EntityFramework
+{
+    /// <summary>
+    /// Interaction logic for AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        MyStoreContext _context = new MyStoreContext();
+        public AdminWindow()
+        {
+            InitializeComponent();
+            LoadCateogoriesIntoTreeView();
+        }
+
+        private void LoadCateogoriesIntoTreeView()
+        {
+            // tao root
+            TreeViewItem root = new TreeViewItem();
+            root.Header = "kho hang Cat Lai";
+            tvCategory.Items.Add(root);
+            // truy van toan bo danh muc
+            var category = _context.Categories.ToList();
+            foreach (var item in category)
+            {
+                TreeViewItem cate_node = new TreeViewItem();
+                cate_node.Header =item.CategoryName;
+                cate_node.Tag =item;
+                root.Items.Add(cate_node);
+                //Loc san pham theo category
+                var products = _context.Products
+                    .Where(x => x.CategoryId==item.CategoryId)
+                    .ToList();
+                //Nap product vao cate tuong ung
+                foreach (var item1 in products)
+                {
+                    //Tao node
+                    TreeViewItem pro_node = new TreeViewItem();
+                    pro_node.Header = item1.ProductName;
+                    pro_node.Tag =item1;
+                    cate_node .Items.Add(pro_node);
+                }
+            }
+            root.ExpandSubtree(); // Mo rong cac cay con
+        }
+
+        private void tvCategory_SelectedItemChanged_1(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if(e.NewValue == null) 
+            {
+                return;
+            }
+            TreeViewItem item = e.NewValue as TreeViewItem; 
+            if (item == null)
+            {
+                return;
+            }
+            Category category = item.Tag as Category;
+            if (category == null)  return;
+            LoadProductIntoListView(category);
+        }
+
+        private void LoadProductIntoListView(Category category)
+        {
+            var products = _context.Products
+                    .Where(x => x.CategoryId == category.CategoryId)
+                    .ToList();
+            lvProduct.ItemsSource = null;
+            lvProduct.ItemsSource = products;
+        }
+    }
+}
---
+++ MyStoreWpfApp_EntityFramework/LoginWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using MyStoreWpfApp_EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyStoreWpfApp_EntityFramework
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        MyStoreContext context = new MyStoreContext();
+        public LoginWindow()
+        {
+            InitializeComponent();
+            ChangeBackground();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string pass = txtPassword.Password;
+            AccountMember am =  context.AccountMembers
+                .FirstOrDefault(m => m.EmailAddress == email && m.MemberPassword == pass);
+            if (am == null)
+            {
+                MessageBox.Show("Login Failed","Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                if (am.MemberRole == 1)
+                {
+                    MessageBox.Show("Login as ADMIN successfully", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    AdminWindow adminWindow = new AdminWindow();
+                    adminWindow.Show();
+                    Close();
+                    return;
+                }
+                else if (am.MemberRole == 2)
+                {
+                    MessageBox.Show("Login as STAFF successfully", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                else if (am.MemberRole == 3)
+                {
+                    MessageBox.Show("Login as MEMBER successfully", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return ;
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void ChangeBackground()
+        {
+            LinearGradientBrush brush = new LinearGradientBrush();
+            brush.GradientStops.Add(new GradientStop(Colors.Magenta, 0.0));
+            brush.GradientStops.Add(new GradientStop(Colors.Yellow, 0.5));
+            brush.GradientStops.Add(new GradientStop(Colors.Cyan, 1.0));
+            btnExit.Background = brush;
+            //
+            RadialGradientBrush brush2 = new RadialGradientBrush();
+            brush2.GradientOrigin = new Point(0.5, 0.75);
+            brush2.GradientStops.Add(new GradientStop(Colors.Red,0.0));
+            brush2.GradientStops.Add(new GradientStop(Colors.White, 0.5));
+            brush2.GradientStops.Add(new GradientStop(Colors.Blue, 1.0));
+            btnLogin.Background = brush2;
+        }
+    }
+}
